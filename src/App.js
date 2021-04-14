552c3b52f9dfafb7ca918cd03ffad31b935dee49
anas11cs <annus.waheed01@gmail.com>
2021-04-15 01:07:35 +0500

    More WorkoutButton

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function App() {
   return (
@@ -9,9 +12,28 @@ function App() {
       <header className="App-header">
         {/* Below is to convert button into link by just adding href */}
         {/* <Button href="#" variant="contained" color="default"> */}
-        <Button href="#" variant="contained" color="default">
-          Hello World
-        </Button>
+        {/* <Button onClick={()=>alert('Hello')} variant="contained" color="default"> */}
+        {/* <Button
+        style={{
+          fontSize: 24
+        }}
+        variant="contained" color="default">*/}
+        <ButtonGroup variant={"contained"} /*color="default"*/>
+            <Button
+            startIcon={<SaveIcon/>}
+            // endIcon={<SaveIcon/>}
+            // variant="contained"
+            color="primary">
+            Save
+            </Button>
+            <Button
+            startIcon={<DeleteIcon/>}
+            // endIcon={<SaveIcon/>}
+            // variant="contained"
+            color="secondary">
+            Discard 
+            </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
