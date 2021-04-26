b0d85411c5bc155ccd7c5f992d38d008f0f87666
anas11cs <annus.waheed01@gmail.com>
2021-04-26 17:55:21 +0500

    Styled Button-example

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Checkbox  from '@material-ui/core/Checkbox';
 import FormControlLabel  from '@material-ui/core/FormControlLabel';
+import TextField  from '@material-ui/core/TextField';
+import {makeStyles} from '@material-ui/core/styles'
+
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg,#FE6B8B,#FF8E53)',
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color:'white',
+    padding: '5px 30px'
+  }
+})
+
+function ButtonStytled(){
+  const classes = useStyles();
+  return <Button className={classes.root}>Styled Button</Button>
+}
 
 function CheckBoxExample(){
   const [checked, setChecked] = React.useState(true)
@@ -39,6 +58,16 @@ function App() {
           fontSize: 24
         }}
         variant="contained" color="default">*/}
+        <ButtonStytled/>
+        <TextField
+        variant="filled"
+        color="secondary"
+        //type="date"
+        //type="time"
+        type="email"
+        placeholder="emailxyz@email.com"
+        //label="Time Text"
+        />
         <CheckBoxExample/>
         <ButtonGroup variant={"contained"} /*color="default"*/>
             <Button
